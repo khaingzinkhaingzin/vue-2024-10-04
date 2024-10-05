e7843373a025b9749040600277360ba47d13f240
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-10-05 15:11:46 +0630

    list rendering using v-for directive

--- vue3-basic/app.js
+++ vue3-basic/app.js
@@ -1,22 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: "you don't know JS",
-            author: "Ptr",
-            age: 28,
+            books: [
+                {
+                    title: "you don't know JS",
+                    author: "Ptr",
+                    age: 27,
+                },
+                {
+                    title: "Book 2",
+                    author: "Sonya",
+                    age: 28,
+                },
+                {
+                    title: "Book 2",
+                    author: "Lookmhee",
+                    age: 29,
+                }
+            ],
             showBook: true,
         }
     },
     methods: {
-        mouseoverHandler(event, data) {
-            console.log("mouse over is working", event, data); 
-        },
-        mouseleaveHandler(event) {
-            console.log("mouse leave is working", event); 
-        },
-        dblclickHandler(event) {
-            console.log("dblclick is working", event); 
-        }
     }
 });
 
