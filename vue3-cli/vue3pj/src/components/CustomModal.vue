59fbfd2130b29c6f44e7966aeea03a8ed96d314a
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-10-06 21:07:59 +0630

    .self event modifier for event action scope

--- vue3-cli/vue3pj/src/components/CustomModal.vue
+++ vue3-cli/vue3pj/src/components/CustomModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="backdrop" @click="closeModal">
+  <div class="backdrop" @click.self="closeModal">
     <div class="modal" :class="{success: theme === 'success', delete: theme === 'delete'}">
         <h1>{{ header }}</h1>
         <p>{{ content }}</p>
