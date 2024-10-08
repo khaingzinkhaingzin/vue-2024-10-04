943f614c0266d3fb1594bd8bca998fc9a7210683
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-10-08 22:09:25 +0630

    disable play button when playing

--- vue3-cli/reaction_timer_pj/src/App.vue
+++ vue3-cli/reaction_timer_pj/src/App.vue
@@ -1,12 +1,32 @@
 <template>
-  <h1>How fast can you catch me?</h1>
+  <div>
+    <h1>How fast can you catch me?</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+
+    <div v-if="isPlaying">
+      <Block :delay="delay" />
+    </div>
+  </div>
 </template>
 
 <script>
-
+import Block from './components/Block.vue'
 export default {
   name: 'App',
   components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000; // between 2 second and 7 second
+    }
   }
 }
 </script>
---
+++ vue3-cli/reaction_timer_pj/src/components/Block.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="block">Click here</div>
+</template>
+
+<script>
+export default {
+    props: ['delay']
+}
+</script>
+
+<style>
+.block {
+    width: 400px;
+    height: 300px;
+    margin: 20px auto;
+    padding-top: 30px;
+    background-color: aquamarine;
+}
+</style>
