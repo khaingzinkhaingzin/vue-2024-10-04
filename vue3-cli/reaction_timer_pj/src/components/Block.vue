8b1478a827c46c0d6ea9a72c072e7a660340e6c5
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-10-09 20:56:09 +0630

    mounted life cycle for listen event when component was injected into index.html. in other way, document.loaded

--- vue3-cli/reaction_timer_pj/src/components/Block.vue
+++ vue3-cli/reaction_timer_pj/src/components/Block.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="block">Click here</div>
+  <div class="block" v-if="showBlock">Click here {{ delay }}</div>
 </template>
 
 <script>
 export default {
-    props: ['delay']
+    props: ['delay'],
+    data() {
+      return {
+        showBlock: false,
+      }
+    },
+    mounted() { // when component has been injected into index.html
+      setTimeout(() => {
+        this.showBlock = true;
+      }, this.delay);
+    },
+    updated() { // when some data has updated
+      console.log('data updated');
+    },
+    unmounted() { // when component has been removed
+      console.log("component unmounted");
+      
+    }
 }
 </script>
 
